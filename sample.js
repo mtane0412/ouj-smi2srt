37eb95f2274acd68bc312b381638e4fafa709fa9
mtane0412 <mtane0412@gmail.com>
2018-11-22 20:29:39 +0900

    Remove: sample.js

--- sample.js
+++
@@ -1,6 +0,0 @@
-const jschardet = require('jschardet');
-const iconv = require('iconv-lite');
-const fs = require('fs');
-const path = require('path');
-
-console.log(path.parse('SAMI.htm.srt').name);
